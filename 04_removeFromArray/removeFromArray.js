862dc8360ea7cce3a8bf14fb288586ea6b81b94b
Grolen <pein091@gmail.com>
2024-05-14 12:22:45 -0400

    done 4th exercise (I'm a geniuos!!!)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    const arrayOfArgs = [...args];
+    for (let i = 0; i < arrayOfArgs.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if(array[j] === arrayOfArgs[i]) {
+                array.splice(j, 1);
+                j--;
+            }
+        }  
+    }
+    return array;
 };
 
 // Do not edit below this line
