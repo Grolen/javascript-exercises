dbaece76e37b6f790d8c9617b39ed9034135f216
Grolen <pein091@gmail.com>
2024-05-14 12:38:15 -0400

    done with 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    let sum = 0;
+    if (Number.isInteger(numOne) && Number.isInteger(numTwo) && numOne >= 0 && numTwo >= 0) {
+        if(numTwo < numOne) {
+            sum = numTwo
+            for (let i = numTwo + 1; i <= numOne; i++) {
+                sum += i;
+            }
+        } else {
+            sum = numOne
+            for (let i = numOne + 1; i <= numTwo; i++) {
+                sum += i;
+            }
+        }
+    } else {
+        return "ERROR"
+    }
+    return sum
 };
 
 // Do not edit below this line
